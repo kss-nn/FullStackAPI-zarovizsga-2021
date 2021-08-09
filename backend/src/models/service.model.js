6ae9e80903dcdbf3dd83650a647419d039f44cba
kss-nn <67142065+kss-nn@users.noreply.github.com>
2021-08-09 17:10:54 +0200

    kész

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,33 @@
 // 1. Feladat: pótold a service modellt.
+
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    street: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
